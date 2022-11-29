f5c5dc24cf2e9cdab21b7759f7f8eb13c0090a19
srini-equitasit <srini@equitasitinc.com>
2022-11-29 11:01:21 +0530

    initial project

---
+++ src/main/java/com/equitasit/ms/dept/DeptMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.equitasit.ms.dept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equitasit/ms/dept/DeptMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equitasit.ms.dept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeptMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
